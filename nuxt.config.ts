bc975f98c491b4ac79c985ae840726b2909edbb2
Sohaib Alslamt <sohep.alslamat91@gmail.com>
2024-08-26 01:57:34 +0300

    Add baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: false,
+  app: {
+    baseURL: '/cornerstone3D-with-Nuxt3/'
+  },
   vite:{
     resolve: {
       alias: {
